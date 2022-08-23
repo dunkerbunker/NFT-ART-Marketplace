b737c08bc92fa71cdc3c0ba3601d2ab2084b37d6
Yoosuf Sayyid <cena26657@gmail.com>
2022-08-23 11:44:44 +0500

    kk

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
 import Head from 'next/head';
 import Image from 'next/image';
 
-export default function Home() {
-  return (
-   <div>
+const Home = () => (
+  <div>
     <h1 className="text-3xl font-bold underline">
       Hello World
     </h1>
-   </div>
-  )
-}
+  </div>
+);
+
+export default Home;
