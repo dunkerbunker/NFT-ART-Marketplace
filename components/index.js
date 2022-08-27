e9219ac6a7a73d02d9bb1df7b22465f3cd60536a
Yoosuf Sayyid <cena26657@gmail.com>
2022-08-27 20:44:57 +0500

    banner

--- components/index.js
+++ components/index.js
@@ -1,5 +1,7 @@
+/* eslint-disable import/no-cycle */
 import Navbar from './Navbar';
 import Footer from './Footer';
 import Button from './Button';
+import Banner from './Banner';
 
-export { Navbar, Footer, Button };
+export { Navbar, Footer, Button, Banner };
