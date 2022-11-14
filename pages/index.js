8d9cc34bb742d84b6c7e11d1d8fc415e1d975bdd
Yoosuf Sayyid <cena26657@gmail.com>
2022-11-14 19:01:46 +0500

    dads

--- pages/index.js
+++ pages/index.js
@@ -224,7 +224,7 @@ const Home = () => {
                       nft={nft}
                     />
                   ))}
-                  {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) => (
+                  {/* {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) => (
                     <NFTCard
                       key={`nft-${i}`}
                       nft={{
@@ -236,7 +236,7 @@ const Home = () => {
                         description: 'cool NFT on Sale',
                       }}
                     />
-                  ))}
+                  ))} */}
                 </div>
               </div>
             </>
